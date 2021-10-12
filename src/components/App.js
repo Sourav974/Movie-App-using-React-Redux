91ad066afc493bf80eb67893ef90385eb1bf8346
Sourav974 <souravyadav97@gmail.com>
2021-10-12 12:05:46 +0530

    Project setup

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App">Project Setup</header>
+    </div>
+  );
+}
+
+export default App;
