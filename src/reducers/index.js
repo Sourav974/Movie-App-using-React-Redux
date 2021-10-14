fb3f951c3300c75f2027d1cafccae3ffd1ebb589
Sourav974 <souravyadav97@gmail.com>
2021-10-14 14:23:31 +0530

    Add Reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+import { combineReducers } from "redux";
 import {
   ADD_MOVIES,
   ADD_FAVOURITE,
@@ -11,7 +12,8 @@ const initialMoviesState = {
   showFavourites: false,
 };
 
-export default function movies(state = initialMoviesState, action) {
+export function movies(state = initialMoviesState, action) {
+  console.log("MOVIES REDUCER");
   // if (action.type === ADD_MOVIES) {
   //   return {
   //     ...state,
@@ -51,3 +53,29 @@ export default function movies(state = initialMoviesState, action) {
       return state;
   }
 }
+
+const initialSearchState = {
+  result: {},
+};
+
+export function search(state = initialSearchState, action) {
+  console.log("SEARCH REDUCER");
+  return state;
+}
+
+const initialRootState = {
+  movies: initialMoviesState,
+  search: initialSearchState,
+};
+
+// export default function rootReducer(state = initialRootState, action) {
+//   return {
+//     movies: movies(state.movies, action),
+//     search: search(state.search, action),
+//   };
+// }
+
+export default combineReducers({
+  movies,
+  search,
+});
